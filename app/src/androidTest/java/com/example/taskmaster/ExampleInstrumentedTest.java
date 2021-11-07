1ceecb4c77620cd3952c8d89e77c95fc9c7961a6
Heba1998 <hebaalmomani1998@gmail.com>
2021-11-07 20:25:41 +0200

    lab31 done

--- app/src/androidTest/java/com/example/taskmaster/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/taskmaster/ExampleInstrumentedTest.java
@@ -1,10 +1,20 @@
 package com.example.taskmaster;
 
-import android.content.Context;
 
-import androidx.test.InstrumentationRegistry;
-import androidx.test.runner.AndroidJUnit4;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -17,10 +27,62 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> rule = new ActivityScenarioRule<>(MainActivity.class);
+
     @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.taskmaster", appContext.getPackageName());
+    public void ButtonsTest() {
+        onView(withText("All tasks")).check(matches(isDisplayed()));
+        onView(withText("Add tasks")).check(matches(isDisplayed()));
+        onView(withText("Settings")).check(matches(isDisplayed()));
     }
+
+    @Test
+    public void AddTaskTest(){
+        onView(withId(R.id.add)).perform(click());
+        onView((withId(R.id.titletask))).perform(typeText("Lab 31"),closeSoftKeyboard());
+        onView(withId(R.id.bodytask)).perform(typeText("Add Espresso to your application."),closeSoftKeyboard());
+        onView(withId(R.id.statetask)).perform(typeText("In Progress"),closeSoftKeyboard());
+        onView(withId(R.id.Button3)).perform(click());
+        onView(withText("Lab 31")).check(matches(isDisplayed()));
+        onView(withText("Add Espresso to your application.")).check(matches(isDisplayed()));
+        onView(withText("In Progress")).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void AllTasksTest() {
+        onView(withId(R.id.all)).check(matches(isDisplayed())).perform(click());
+        onView(withText("All Tasks")).check(matches(isDisplayed()));
+        onView(withId(R.id.Home)).check(matches(isDisplayed())).perform(click());
+    }
+
+    @Test
+    public void RecyclerViewTest(){
+        onView(withId(R.id.recycleViewId)).perform(click());
+        onView(withId(R.id.title)).check(matches(isDisplayed()));
+        onView(withId(R.id.body)).check(matches(isDisplayed()));
+        onView(withId(R.id.state)).check(matches(isDisplayed()));
+    }
+
+
+//    @Test
+//    public void SettingTest() {
+//        onView(withId(R.id.Settings)).check(matches(isDisplayed())).perform(click());
+//        onView(withId(R.id.editPersonName)).check(matches(isDisplayed())).perform(typeText("Heba"));
+//        onView(withId(R.id.save)).check(matches(isDisplayed())).perform(click(),closeSoftKeyboard());
+//        onView(withId(R.id.GoToHomePage)).check(matches(isDisplayed())).perform(click());
+//        onView(withText("Heba’s tasks")).check(matches(isDisplayed()));
+//    }
+
+
+     public static final String username = "Heba";
+    @Test
+    public void SettingTest() {
+        onView(withId(R.id.Settings)).perform(click());
+        onView(withId(R.id.editPersonName)).perform(typeText(username));
+        onView(withId(R.id.save)).perform(click(),closeSoftKeyboard());
+        onView(withId(R.id.GoToHomePage)).check(matches(isDisplayed())).perform(click());
+        onView(withText(username+"’s tasks")).check(matches(isDisplayed()));
+}
+
 }
