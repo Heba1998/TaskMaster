49f07d653ec0394f17d419a0b141d240b73c5fcc
Heba1998 <hebaalmomani1998@gmail.com>
2021-11-22 23:35:57 +0200

    finish lab42 wiyhout readme file

--- app/src/main/java/com/amplifyframework/datastore/generated/model/AmplifyModelProvider.java
+++ app/src/main/java/com/amplifyframework/datastore/generated/model/AmplifyModelProvider.java
@@ -13,7 +13,7 @@ import java.util.Set;
  */
 
 public final class AmplifyModelProvider implements ModelProvider {
-  private static final String AMPLIFY_MODEL_VERSION = "a5885f7c7cba4e86e2ea19a5b2e52b66";
+  private static final String AMPLIFY_MODEL_VERSION = "5a59959875359552e886fe589aff4579";
   private static AmplifyModelProvider amplifyGeneratedModelInstance;
   private AmplifyModelProvider() {
     
